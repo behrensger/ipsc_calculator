78e3b8224e985a6647ad26fe04f5cd165674e61a
behrensger <tukaram@openaqua.de>
2020-10-02 01:25:26 +0200

    init

---
+++ build/precache-manifest.139ee649b7775fa97faa818cdfef0c80.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7de8fa9ce0aacacf20d8",
+    "url": "/static/js/main.bb290f4c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "abc921c608ebbf2c207b",
+    "url": "/static/js/2.fdc7f0fe.chunk.js"
+  },
+  {
+    "revision": "566fe5b9e14b69934a61e19617d69581",
+    "url": "/index.html"
+  }
+];
---
+++ build/static/js/main.bb290f4c.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,n,o){e.exports=o(4)},function(e,n,o){"use strict";o.r(n);var t=o(0),r=o.n(t),c=o(2);o.n(c).a.render(r.a.createElement("p",null,"Hello world!!!"),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.bb290f4c.chunk.js.map
